da83479e15733daf8d22976f7c1c3a1abe3ddc68
Sungmin Cho <sam020930@gmail.com>
2025-06-05 17:40:51 +0900

    Initialized project skeleton
    Co-authored-by: Sujin Shin <shin369123@gmail.com>

--- README.md
+++ README.md
@@ -9,8 +9,11 @@ API of internet famous cats
 - **Local Storage** - Storing actual GIFs (or links from Giphy)
 
 ## MVP Features
-- **`GET /gifs`** - Retrieve a list of all cat GIF memes  
-- **`GET /gifs/{id}`** - Retrieve details of a specific cat GIF meme  
+- **`GET /gifs`** - Retrieve a list of all cat GIF meme alias 
+- **`GET /gifs/random`** - Retrieve a random cat GIF meme 
+- **`GET /gifs/{alias}`** - Retrieve details of a specific cat GIF meme  
+
+**For authorized users only**
 - **`POST /gifs`** - Upload a new cat GIF meme (via file upload or URL)  
 - **`PUT /gifs/{id}`** - Update metadata of a specific GIF meme  
 - **`DELETE /gifs/{id}`** - Delete a specific GIF meme from the collection
--- app/main.py
+++ app/main.py
@@ -1,31 +1,70 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.responses import PlainTextResponse
 
-app = FastAPI()
+# Create FastAPI app
+app = FastAPI(
+    title='Happy Cat API', 
+    description='A simple API for internet famous cats!',
+    version="1.0.0"
+)
 
-class Item(BaseModel):
-    text: str = None
-    is_done: bool = False
+# class Item(BaseModel):
+#     catID: int = None
+#     alias: str = None
+#     url:   str = None
+    
 
-items = []
-
-@app.get("/")
+'''
+    GET /: Welcome endpoint
+'''
+@app.get("/", response_class=PlainTextResponse)
 def root():
-    return {"Hello": "Giyami"}
+    return """^>⩊<^ Welcome to Happy Cat API ^>⩊<^
+This API directs you to the GIF of world-widely renowned cats
+there to make your day.
 
-@app.post("/items")
-def create_item(item: Item):
-    items.append(item)
-    return items
+Available methods:
+GET /gifs - Retrieve a list of all cat GIF memes  
+GET /gifs/{id} - Retrieve details of a specific cat GIF meme  
+PUT /gifs/{id} - Update metadata of a specific GIF meme  
+DELETE /gifs/{id} - Delete a specific GIF meme from the collection
+"""
 
-@app.get("/items")
-def get_all() -> list[Item]:
-    return items
 
-@app.get("/items/{item_id}")
-def get_item(item_id: int) -> Item:
-    if item_id < len(items):
-        return items[item_id]
-    else:
-        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+@app.get("/happycat")
+def get_happy_cat():
+    return {
+        "catID": 0, 
+        "alias": "happycat", 
+        "url": "https://tenor.com/bXAn9.gif"
+    }
+    # return Item(0, "happycat", "https://tenor.com/bXAn9.gif")
+
+
+# class Item(BaseModel):
+#     text: str = None
+#     is_done: bool = False
+
+# items = []
+
+# @app.get("/")
+# def root():
+#     return {"Hello": "Giyami"}
+
+# @app.post("/items")
+# def create_item(item: Item):
+#     items.append(item)
+#     return items
+
+# @app.get("/items")
+# def get_all() -> list[Item]:
+#     return items
+
+# @app.get("/items/{item_id}")
+# def get_item(item_id: int) -> Item:
+#     if item_id < len(items):
+#         return items[item_id]
+#     else:
+#         raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
 
