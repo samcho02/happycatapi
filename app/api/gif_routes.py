689b757bde943e84fff56c90d61a643be62bee8b
Sungmin Cho <sam020930@gmail.com>
2025-06-06 15:57:44 +0900

    refactor: move helpers to core.utils
    Co-authored-by: Sujin Shin <shin369123@gmail.com>

--- app/api/gif_routes.py
+++ app/api/gif_routes.py
@@ -1,43 +1,26 @@
 from fastapi import APIRouter, HTTPException
-import random
+from app.db.gifs_test_db import gifs_test_db
+from app.core.utils import gif_service
 
 router = APIRouter()
-
-gifs_test_db = {
-    "happycat": {
-        "id": "0",
-        "url": "https://tenor.com/bXAn9.gif"
-    },
-    "carla": {
-        "id": "1",
-        "url": "https://tenor.com/rJ4PNMf6dC5.gif"
-    },
-    "ripcarla": {
-        "id": "2",
-        "url": "https://tenor.com/b78eKUM95k3.gif"
-    },
-    "huhcat": {
-        "id": "3",
-        "url": "https://tenor.com/sqMU1WMDcgD.gif"
-    }
-}
+gif_service = gif_service(gifs_test_db)
 
 # get all gifs in the test db
 @router.get("/")
 async def get_all_gifs():
-    return list(gifs_test_db.values())
+    return gif_service.get_all_gifs()
 
 # get random gif
 @router.get("/random")
 async def get_random_gif():
-    return random.choice(list(gifs_test_db.values()))
+    return gif_service.get_random_gif()
 
 # get gif by tag
 @router.get("/{tag}")
 async def get_gif_by_tag(tag: str):
-    gif = gifs_test_db.get(tag.lower())
+    gif = gif_service.search_by_tag(tag)
 
-    if not gif:  # no gif of the tag
+    if not gif:
         raise HTTPException(status_code=404, detail="GIF not found") 
     
-    return gif 
+    return gif
